6b5687967c0693ce67aab4c14a4078bcb834251c
ninjaproject <nirajkumarv1997@gmail.com>
2021-01-06 15:23:07 +0530

    login done

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,9 +18,10 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrustProxies::class,
         \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
-        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+        \Illuminate\Session\Middleware\StartSession::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\userauth::class,
     ];
 
     /**
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,16 +3,17 @@
 <div class="container custome-login">
     <div class="row justify-content-center mt-3">
         <div class="col-sm-4 col-sm-offset-4 ">
-        <form>
+        <form action="login" method="POST">
+        @csrf
             <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <label for="exampleInputEmail1"  class="form-label">Email address</label>
+                <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp">
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <input type="password" class="form-control" name="password" id="exampleInputPassword1">
             </div>
-           <button type="submit" class="btn btn-primary">Submit</button>
+           <button type="submit" class="btn btn-primary">Login</button>
         </form>
             
         </div>
